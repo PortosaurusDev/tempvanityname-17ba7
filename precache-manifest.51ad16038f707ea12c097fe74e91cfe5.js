4e15007fe779789ffa4d1e25cde7f9f15772328d
PortosaurusBot <claperone.portasaurus@gmail.com>
2020-06-02 23:37:04 +0000

    Auto generated portfolio

---
+++ precache-manifest.51ad16038f707ea12c097fe74e91cfe5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa8c5655c0073c0ed2623cad23a65bc",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/index.html"
+  },
+  {
+    "revision": "41faf3723771a0faaf50",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6bb64744ce24d0b410f1",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/static/js/2.18e4ac39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/static/js/2.18e4ac39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41faf3723771a0faaf50",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/static/js/main.aaf2375c.chunk.js"
+  },
+  {
+    "revision": "e8e36b8b64bf576381ac",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/static/js/runtime-main.25992e92.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "https://portosaurusdev.github.io/tempvanityname-17ba7/static/media/logo.5d5d9eef.svg"
+  }
+]);
